2180b1cc888ad44f36e2d1c7f42cbe9d38fcbbc6
Akinoe27 <140145591+Akinoe27@users.noreply.github.com>
2023-11-25 15:33:38 +0800

    update

--- ASSET/JS/index.js
+++ ASSET/JS/index.js
@@ -1,52 +1,27 @@
-// let imageTrigger = document.querySelector("#triggerImage1");
-// let modalContainer = document.querySelector("#enlargeImage1");
-// let modalImageSource = document.querySelector("#modalImage");
-
-// // variable for bootstrap css modal
-// let bootstrapModal = new bootstrap.Modal(modalContainer)
-// function showImageInmodal(){
-//     modalImageSource.src = this.src
-//     modalImageSource.className = "d-block mx-auto w-100"
-//     bootstrapModal.show();
-// }
-
-// // add event to variable that refer to the original image
-// imageTrigger.addEventListener("click", showImageInmodal)
-
-// Act2
-// let buttonTrigger = document.querySelector("#triggerButton");
-// let ContainerModal = document.querySelector("#showQuantity");
-// let modalButtonSource = document.querySelector("#modalButton");
-
-// // variable for bootstrap css modal
-// let bootstrapModal = new bootstrap.Modal(ContainerModal)
-// function showQuantityInmodal(){
-//     modalButtonSource.src = this.src
-//     bootstrapModal.show();
-// }
-
-// // add event to variable that refer to the original image
-// buttonTrigger.addEventListener("click", showQuantityInmodal)
-
-
-
-
-
 document.addEventListener("DOMContentLoaded",function (){
-  document.querySelectorAll("#Image").forEach((element)=>{
-element.addEventListener("click", function (){
+  document.querySelectorAll(".img-fluid").forEach((imgDesc)=>{
+    imgDesc.addEventListener("click", function (){
     document.querySelector("#modalImage").src = this.src;
 })
   })
 });
 
-// document.addEventListener("DOMContentLoaded",function (){
-//     document.querySelectorAll("#Button").forEach((element)=>{
-//         element.addEventListener("click", function (){
-//             document.querySelector("#modalButton").src = this.src;
-//         })
-//           })
-//     })
+document.addEventListener("DOMContentLoaded",function (){
+    document.querySelectorAll(".btn").forEach((orderBtn)=>{
+      orderBtn.addEventListener("click", function (){
+            let parentContainer = orderBtn.closest(".col-lg-6");
+            let ProductImg = parentContainer.querySelector(".img-fluid").src; 
+            let itemPrice = parentContainer.querySelector("#price").textContent; 
+            console.log(itemPrice)
+            document.querySelector("#productImage").src = ProductImg;
+            document.querySelector("#priceModal").textContent = itemPrice;
+
+
+        });
+          });
+    });
+
+  
    
  
 
