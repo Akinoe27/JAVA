67764e4c2f4bcaa90fe8313509fd53ec28741dd9
Akinoe27 <elder_aquino27@yahoo.com>
2023-11-28 16:01:50 +0800

    updated

--- ASSET/JS/index.js
+++ ASSET/JS/index.js
@@ -8,17 +8,27 @@ document.addEventListener("DOMContentLoaded",function (){
 
 document.addEventListener("DOMContentLoaded",function (){
     document.querySelectorAll(".btn").forEach((orderBtn)=>{
-      orderBtn.addEventListener("click", function (){
-            let parentContainer = orderBtn.closest(".col-lg-6");
-            let ProductImg = parentContainer.querySelector(".img-fluid").src; 
-            let itemPrice = parentContainer.querySelector("#price").textContent; 
-            console.log(itemPrice)
-            document.querySelector("#productImage").src = ProductImg;
-            document.querySelector("#priceModal").textContent = itemPrice;
+      orderBtn.addEventListener("click", function () {
+        let parentContainer = orderBtn.closest(".col-lg-3");
+        let ProductImg = parentContainer.querySelector(".img-fluid").src;
+        let itemPrice = parentContainer.querySelector("#price").textContent;
+        console.log(itemPrice);
+        document.querySelector("#productImage").src = ProductImg;
+        document.querySelector("#priceModal").textContent = itemPrice;
+        // Quantity / Price Reset
+        document.querySelector("#quantity").value = "";
+        document.querySelector("#totalPrice").textContent = "0.00";
+      });
+    });
+    document.querySelector("#quantity").addEventListener("input", () => {
+      // SRP Price
+      
+      let price = 10
+      let result = document.querySelector ("#quantity").value
+      let totalPrice = result * price;
+      document.querySelector("#totalPrice").textContent = totalPrice.toFixed(2);
 
-
-        });
-          });
+    });
     });
 
   
